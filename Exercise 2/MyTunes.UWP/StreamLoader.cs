c0d00cb122629cfacbecc6ae86da7f06eecfba94
Israel Arias Fallas <israel.arias@grupobabel.com>
2017-10-30 00:45:21 -0600

    XAM110 - Exercise #2

---
+++ Exercise 2/MyTunes.UWP/StreamLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyTunes.Shared;
+using System.IO;
+namespace MyTunes
+{
+    public class StreamLoader: IStreamLoader
+    {
+        public Stream GetStreamForFilename(string filename)
+        {
+            return Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(filename).AsTask()
+                .Result.OpenStreamForReadAsync().Result;
+        }
+    }
+}
---
+++ Exercise 2/MyTunes.iOS/StreamLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyTunes.Shared;
+using System.IO;
+
+namespace MyTunes
+{
+   public class StreamLoader: IStreamLoader
+    {
+        public Stream GetStreamForFilename(string filename)
+        {
+            return File.OpenRead(filename);
+        }
+    }
+}
