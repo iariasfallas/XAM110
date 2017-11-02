72fbcf936fb9c6e753a1b4005622a39f5b15d98d
Israel Arias Fallas <israel.arias@grupobabel.com>
2017-11-02 00:45:48 -0600

    XAM110 - Exercise #4

--- Exercise 2/MyTunes.NetStandard/SongExtensions.cs
+++ Exercise 2/MyTunes.NetStandard/SongExtensions.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Net.Http;
+//using System.Net.Http;
 namespace MyTunes.NetStandard   
 {
     public static class SongExtensions
     {
-        public static HttpClient httpClient = new HttpClient();
+        //public static HttpClient httpClient = new HttpClient();
         public static string RuinSongName(this string songName)
         {
             return songName.Replace("Cocodrile", "Alligator");
---
+++ Exercise 2/MyTunes.NetStandard/SongLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+using System.Reflection;
+using MyTunes.Shared;
+using MyTunes.NetStandard;
+
+namespace MyTunes
+{
+	public static class SongLoader
+	{
+		const string Filename = "songs.json";
+        public static IStreamLoader Loader { get; set; }
+		public static async Task<IEnumerable<Song>> ImprovedLoad()
+		{
+			using (var reader = new StreamReader(OpenData())) {
+				var songs =  JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
+                foreach(Song song in songs) { song.Name = song.Name.RuinSongName(); }
+                return songs;
+			}
+		}
+
+		private static Stream OpenData()
+		{
+            if(Loader == null)
+            {
+                throw new Exception("Must set platform Loader before calling Load.");
+            }
+
+            return Loader.GetStreamForFilename(Filename);
+        }
+	}
+}
+
